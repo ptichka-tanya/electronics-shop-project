d518088d9f448d3d1050b5a29626ec94ea71974d
ptichka-tanya <t.n.sinitsina@gmail.com>
2024-01-08 16:43:37 +0300

    Добавлен класс Item и тесты для него

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+
+Item.pay_rate = 0.75
+
+assert Item.all == [item1]
+
+
+def test_calculate_total_price():
+    assert item1.price * item1.quantity == 200000
+    assert item1.calculate_total_price() == 200000
+
+
+def test_apply_discount():
+    assert item1.price * Item.pay_rate == 7500
+
+    item1.apply_discount()
+    assert item1.price == 7500
