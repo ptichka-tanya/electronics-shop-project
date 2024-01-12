f01a59bcf8cc59d984894f33a877c508c9d7e7de
ptichka-tanya <t.n.sinitsina@gmail.com>
2024-01-12 23:41:13 +0300

    Добавлены новые методы для класс Item и внесены изменения в атрибуты класса

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,10 +1,11 @@
 from src.item import Item
 
 item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Парогенератор", 200, 5)
 
 Item.pay_rate = 0.75
 
-assert Item.all == [item1]
+assert Item.all == [item1, item2]
 
 
 def test_calculate_total_price():
@@ -17,3 +18,28 @@ def test_apply_discount():
 
     item1.apply_discount()
     assert item1.price == 7500
+
+
+def test_name_property():
+    assert item1.name == "Смартфон"
+    assert item2.name == "Парогенератор"
+
+
+def test_name_setter():
+    item1.name = 'Смартфон'
+    assert item1.name == 'Смартфон'
+    item2.name = 'Парогенератор'
+    assert item2.name == 'Парогенера'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+
+    assert len(Item.all) == 5
+    assert Item.all[0].name == "Смартфон"
+    assert Item.all[3].price == 50
+    assert Item.all[1].quantity == 3
+
+
+def test_string_to_number():
+    assert Item.string_to_number("4.27465") == 4
